5cfbae24745c2d9af9d248e1509ac7cb16671fad
Navneet Gupta <navneetguptaend@gmail.com>
2022-02-06 19:38:50 +0530

    player-name-updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
-
-
+const formElement = document.querySelector('form');
+const errorOutputElement = document.getElementById('config-error');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
@@ -12,3 +12,6 @@ editPlayer2BtnElement.addEventListener('click',openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener('click',closePlayerConfig);
 backdropElement.addEventListener('click',closePlayerConfig);
+
+
+formElement.addEventListener('submit',savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -6,4 +6,20 @@ function openPlayerConfig(){
 function closePlayerConfig(){
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorOutputElement.textContent = '';
+}
+
+
+function savePlayerConfig(event){
+   event.preventDefault();     // prevent the default from submmission
+    const formData = new FormData(event.target);   // get the form data
+    const enteredPlayerName = formData.get('playername').trim();   //trim used for deleting the whitespaces    //get allows to take the value of the input 
+
+
+    if(!enteredPlayerName){
+        event.target.firstElementChild.classList.add('error');
+      errorOutputElement.textContent = 'Please enter a valid name';
+      return;
+    }
 }
