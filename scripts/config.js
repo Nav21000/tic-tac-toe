a6216158a1ccb4d2c52235ae536dbf4120086a32
Navneet Gupta <navneetguptaend@gmail.com>
2022-02-07 20:30:29 +0530

    Get player name completed

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig(){
+function openPlayerConfig(event){
+    editedPlayer = +event.target.dataset.playerid;  //dataset is used to get the data from the html
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
 }
@@ -8,6 +9,7 @@ function closePlayerConfig(){
     backdropElement.style.display = 'none';
     formElement.firstElementChild.classList.remove('error');
     errorOutputElement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = '';  //clear the input field //this is not the best practise but it works
 }
 
 
@@ -15,11 +17,20 @@ function savePlayerConfig(event){
    event.preventDefault();     // prevent the default from submmission
     const formData = new FormData(event.target);   // get the form data
     const enteredPlayerName = formData.get('playername').trim();   //trim used for deleting the whitespaces    //get allows to take the value of the input 
-
-
+    // console.log(enteredPlayerName);
     if(!enteredPlayerName){
         event.target.firstElementChild.classList.add('error');
       errorOutputElement.textContent = 'Please enter a valid name';
       return;
     }
+
+
+    const updatedPlayerDataElement=document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+
+  players[editedPlayer-1].name = enteredPlayerName;    //editedPlayer-1 is used to get the index of the player //this player is the player that is being edited and store for accessing when to print winners name
+
+  closePlayerConfig(); //close the player config // when done edited done
 }
+
